794f81955c583549d759e8de611e37fb0e0d9693
Daniel Roe <daniel@roe.dev>
2026-05-28 23:07:14 +0100

    refactor(scannor): drop vite + move to rolldown

--- services/scanner/nitro.config.ts
+++ services/scanner/nitro.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from 'nitro'
 
 export default defineConfig({
   serverDir: '.',
-  traceDeps: ['@tensorflow/tfjs-node*'],
+  builder: 'rolldown',
+  traceDeps: [
+    '@tensorflow/tfjs*',
+    '@tensorflow/tfjs-node*',
+    '@tensorflow/tfjs-core*',
+    '@tensorflow/tfjs-converter*',
+    '@tensorflow/tfjs-layers*',
+    '@tensorflow/tfjs-backend-cpu*',
+    '@tensorflow/tfjs-backend-webgl*',
+    '@tensorflow/tfjs-data*',
+    'nsfwjs*',
+  ],
 })
